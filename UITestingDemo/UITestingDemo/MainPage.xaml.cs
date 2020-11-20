025767dd24704a86a0fd58045b1e9b2008325afc
Stefanija <stefanija__popovska@hotmail.com>
2020-11-20 19:21:43 +0100

    Added View Model for registration page

--- UITestingDemo/UITestingDemo/MainPage.xaml.cs
+++ UITestingDemo/UITestingDemo/MainPage.xaml.cs
@@ -1,46 +1,17 @@
-﻿using System;
-using System.Net.Mail;
+﻿using UITestingDemo.ViewModels;
 using Xamarin.Forms;
 
 namespace UITestingDemo
 {
     public partial class MainPage : ContentPage
     {
-        private bool AreFieldsNotNullOrEmpty => string.IsNullOrEmpty(EmailEntry.Text)
-            && string.IsNullOrEmpty(UserNameEntry.Text)
-            && string.IsNullOrEmpty(PasswordEntry.Text)
-            && string.IsNullOrEmpty(ConfirmPasswordEntry.Text);
-
-        private bool DoPasswordsMatch => PasswordEntry.Text == ConfirmPasswordEntry.Text;
-
         public MainPage()
         {
             InitializeComponent();
 
             NavigationPage.SetHasNavigationBar(this, false);
+
+            BindingContext = new MainPageViewModel(Navigation);
         }
-
-        async void Button_Clicked(System.Object sender, System.EventArgs e)
-        {
-            if(!AreFieldsNotNullOrEmpty && IsValidEmail(EmailEntry.Text) && DoPasswordsMatch)
-            {
-                await Navigation.PushAsync(new WelcomePage());
-            }
-        }
-
-        public bool IsValidEmail(string emailaddress)
-        {
-            try
-            {
-                MailAddress mailAddress = new MailAddress(emailaddress);
-
-                return true;
-            }
-            catch (FormatException)
-            {
-                return false;
-            }
-        }
-
     }
 }
---
+++ UITestingDemo/UITestingDemo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Mail;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+
+namespace UITestingDemo.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public INavigation _naviagtion { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool AreFieldsNotNullOrEmpty => string.IsNullOrEmpty(Email)
+            && string.IsNullOrEmpty(Username)
+            && string.IsNullOrEmpty(Password)
+            && string.IsNullOrEmpty(ConfirmPassword);
+
+        private bool DoPasswordsMatch => Password == ConfirmPassword;
+
+        public MainPageViewModel(INavigation navigation)
+        {
+            _naviagtion = navigation;
+
+            HandleRegistrationCommand = HandleRegistrationButtonClicked();
+        }
+
+        public ICommand HandleRegistrationCommand { get; }
+
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+            }
+        }
+
+        private string _confirmPassword;
+        public string ConfirmPassword
+        {
+            get => _confirmPassword;
+            set
+            {
+                _confirmPassword = value;
+            }
+        }
+
+        private ICommand HandleRegistrationButtonClicked()
+        {
+            return new Command(() =>
+            {
+                if (!AreFieldsNotNullOrEmpty && IsValidEmail(Email) && DoPasswordsMatch)
+                {
+                    Action _navigateToMainPage =
+                         async () =>
+                         {
+                             await _naviagtion.PushAsync(new WelcomePage());
+                         };
+                    _navigateToMainPage.Invoke();
+                }
+            });
+        }
+
+        public bool IsValidEmail(string emailaddress)
+        {
+            try
+            {
+                MailAddress mailAddress = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
