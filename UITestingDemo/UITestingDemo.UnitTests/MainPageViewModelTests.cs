d52180724bc4e77ffc86e1700425e2cbf98e63ff
Stefanija <stefanija__popovska@hotmail.com>
2020-11-22 19:27:28 +0100

    added unit test

--- UITestingDemo/UITestingDemo.UnitTests/MainPageViewModelTests.cs
+++ UITestingDemo/UITestingDemo.UnitTests/MainPageViewModelTests.cs
@@ -1,16 +1,16 @@
 using System.Linq;
+using System.Threading.Tasks;
 using Autofac.Extras.Moq;
+using Moq;
 using NUnit.Framework;
 using UITestingDemo.ViewModels;
+using Xamarin.Forms;
 using static Pattern.Tests.XamarinFormsMock;
 
 namespace UITestingDemo.UnitTests
 {
     public class MainPageViewModelTests
     {
-        readonly App _app;
-        readonly WelcomePage _loginPage;
-
         public MainPageViewModelTests()
         {
             Init();
@@ -22,12 +22,14 @@ namespace UITestingDemo.UnitTests
         }
 
         [Test]
-        public async void CreatingHomePageViewModel_WhenSuccessfull_SelectedVideoIsNull()
+        public void CreatingWelcomePageViewModel_WhenSuccessfull_Registration()
         {
             using (var mock = AutoMock.GetLoose())
             {
                 //Arange
                 var viewModel = mock.Create<MainPageViewModel>();
+                var navigationMock = new Mock<INavigation>();
+
 
                 viewModel.Username = "PopovskaS";
                 viewModel.Email = "spo@spo.com";
@@ -42,9 +44,7 @@ namespace UITestingDemo.UnitTests
                 Assert.AreEqual(viewModel.IsValidEmail(viewModel.Email), true);
                 Assert.AreEqual(viewModel.DoPasswordsMatch, true);
 
-                var welcomePage = new WelcomePage();
-                await viewModel._naviagtion.PushAsync(welcomePage);
-                Assert.AreEqual(viewModel._naviagtion.NavigationStack.Last(), welcomePage);
+                navigationMock.Setup(s => s.PushAsync(It.IsAny<WelcomePage>()));
             }
 
         }
